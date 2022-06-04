ba04402063347c9f415a81c95279f2aa66104545
Steven Dorvil <dorvilsteven@gmail.com>
2022-06-03 20:55:37 -0400

    api integration setup complete, get single story and get multiple stories through the api is complete

---
+++ src/utils/apis.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { BASE_API_URL } from './constants';
+
+// get single story
+const getStory = async (id) => {
+    try {
+        const story = await axios.get(`${BASE_API_URL}/item/${id}.json`);
+        return story;
+    } catch (error) {
+        console.log('Error while loading story.');
+    }
+};
+
+export const getStories = async (type) => {
+    try {
+        const { data: storyIds } = await axios.get(`${BASE_API_URL}/${type}stories.json`);
+        const stories = await Promise.all(storyIds.slice(0,30).map(getStory));
+        return stories;
+    } catch (error) {
+        console.log('Error while loading list of stories');
+    }
+};
